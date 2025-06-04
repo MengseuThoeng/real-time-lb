65956caef80cb20b7b7fef5de34b590d3e8ada5e
MengseuThoeng <mengseu2004@gmail.com>
2025-06-04 14:46:11 +0700

    first commit

---
+++ src/main/java/com/reki/realtimelb/RealTimeLbApplication.java
@@ -0,0 +1,13 @@
+package com.reki.realtimelb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeLbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealTimeLbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reki/realtimelb/RealTimeLbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reki.realtimelb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeLbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
